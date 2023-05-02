9cfebf447fb30969f091369301cd447256569222
alsurin <alsurin@ozon.ru>
2023-05-02 14:31:16 +0300

    add ckeck refresh token to bd

--- middlewares/auth-middlewares.ts
+++ middlewares/auth-middlewares.ts
@@ -2,17 +2,19 @@ import express from 'express'
 
 import { TokenService } from "../services/token-service"
 
-export default function (req: express.Request, res: express.Response, next: express.NextFunction) {
+export default async function (req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
         const authorizationHeader = req.headers.authorization
+        const { refreshToken } = req.cookies
 
         if(!authorizationHeader) {
             return next(new Error("Ошибка авторизации"))
         }
 
         const accessToken = (authorizationHeader as string).split(' ')[1]
+        const tokenFromDb = await TokenService.findToken(refreshToken)
 
-        if(!accessToken) {
+        if(!accessToken || !tokenFromDb) {
             return next(new Error("Ошибка авторизации"))
         }
 
